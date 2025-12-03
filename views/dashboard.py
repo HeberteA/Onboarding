22e5fa604c7e2283185ec09d27d9705944af5ec3
HeberteA <heberte.2307@gmail.com>
2025-12-03 13:58:17 -0300

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -145,9 +145,10 @@ def render_dashboard(dm):
         if not pending_df.empty:
             gargalos = pending_df['sector'].value_counts().reset_index().head(5)
             gargalos.columns = ['Setor', 'Qtd']
-            fig_bar = px.bar(gargalos, x='Qtd', y='Setor', orientation='h', color_discrete_sequence=['#E37026'], textposition='outside')
+            fig_bar = px.bar(gargalos, x='Qtd', y='Setor', orientation='h', color_discrete_sequence=['#E37026'], text="Qtd")
             fig_bar = update_fig_layout(fig_bar)
             fig_bar.update_layout(yaxis=dict(autorange="reversed"))
+            fig.update_traces(textposition='outside')
             st.plotly_chart(fig_bar, use_container_width=True)
         else:
             st.success("Sem pendências críticas.")
