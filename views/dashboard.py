e5113cf99d53d2ea5e3ad435ee891c96cde5d108
HeberteA <heberte.2307@gmail.com>
2025-12-03 13:54:22 -0300

    Update dashboard.py

--- views/dashboard.py
+++ views/dashboard.py
@@ -147,8 +147,7 @@ def render_dashboard(dm):
             gargalos.columns = ['Setor', 'Qtd']
             fig_bar = px.bar(gargalos, x='Qtd', y='Setor', orientation='h', color_discrete_sequence=['#E37026'])
             fig_bar = update_fig_layout(fig_bar)
-            fig_bar.update_layout(yaxis=dict(autorange="reversed"))
-            fig_bar.update_traces(textposition='outside')
+            fig_bar.update_layout(yaxis=dict(autorange="reversed", textposition='outside'))
             st.plotly_chart(fig_bar, use_container_width=True)
         else:
             st.success("Sem pendências críticas.")
