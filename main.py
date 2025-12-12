7dfa82844024e656778e08773a5271543ecc52e3
HeberteA <heberte.2307@gmail.com>
2025-12-12 09:57:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,8 @@ def login_screen():
         st.markdown(f"""
         <div class="login-container">
             {header_html}
-            <h2 style='color:#E37026; font-size: 2.25rem; margin-top: 10px; margin-bottom: 0px;'>ONBOARDING</h2>
-            <p style='color:#E37026; font-size: 1.25rem; margin-top: 0px; letter-spacing: 2px;'>Gestão de Obras</p>
+            <h2 style='color:#E37026; font-size: 2.75rem; margin-top: 10px; margin-bottom: 0px;'>ONBOARDING</h2>
+            <p style='color:#E37026; font-size: 1.0rem; margin-top: 0px; letter-spacing: 2px;'>Gestão de Obras</p>
         </div>
         """, unsafe_allow_html=True)
         
