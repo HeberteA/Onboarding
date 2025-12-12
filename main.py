5ed4eec37050de00fd43d544032730e5cc461878
HeberteA <heberte.2307@gmail.com>
2025-12-12 09:29:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ st.markdown("""
         background-color: transparent; 
         background-image: linear-gradient(160deg, #1e1e1f 0%, #0a0a0c 100%);
         border: 1px solid rgba(255, 255, 255, 0.1);
-        padding: 20px;
+        padding: 50px;
         border-radius: 10px;
         text-align: center;
         margin-bottom: 20px;
